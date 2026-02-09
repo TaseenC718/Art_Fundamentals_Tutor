2584cefe4c9d3c915821c15ee36f70ceacdd7ebb
ajiangny <andrewjiang74@gmail.com>
2026-02-08 19:12:54 -0500

    Add screenshot and configure for GH Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -14,6 +14,7 @@ export default defineConfig(({ mode }) => {
       alias: {
         '@': path.resolve(__dirname, './src'),
       }
-    }
+    },
+    base: '/Art_Fundamentals_Tutor/',
   };
 });
